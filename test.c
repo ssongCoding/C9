54ae431feffa496f327c582e79944330d01abe19
ssongCoding <songa.kim@kakao.com>
2022-07-04 16:13:42 +0900

    C언어 1주차 수업 코드(Day02~Day06)

--- test.c
+++ test.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
-int main(void) {
-
-	printf("hello world");
-	printf("How does it work?");
-
-	printf("1. 연필 클릭 (Commit) -> 메세지 작성 -> 모두 커밋 클릭");
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main(void) {
+//
+//	printf("1. 연필 클릭 (Commit) -> 메세지 작성 -> 모두 커밋 클릭");
+//	printf("2. 위로 올라가는 화살표 클릭 (Push) -> 커밋한 내용 클릭 -> 푸시 클릭");
+//
+//	return 0;
+//}
