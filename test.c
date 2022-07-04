da105a3c5abfd43839b6374d19a31559606af944
ssongCoding <songa.kim@kakao.com>
2022-07-04 16:30:23 +0900

    Day02~Day06

--- test.c
+++
@@ -1,9 +0,0 @@
-//#include <stdio.h>
-//
-//int main(void) {
-//
-//	printf("1. 연필 클릭 (Commit) -> 메세지 작성 -> 모두 커밋 클릭");
-//	printf("2. 위로 올라가는 화살표 클릭 (Push) -> 커밋한 내용 클릭 -> 푸시 클릭");
-//
-//	return 0;
-//}
